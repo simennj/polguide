6efe3944d00ea95ff5bae0140ad71b8f42e055a8
Simen Norderud jensen <simennj@outlook.com>
2017-08-30 16:39:24 +0200

    use prod settings unless DJANGO_DEVELOPMENT env var is None

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polguide.settings.dev")
+    if os.environ.get('DJANGO_DEVELOPMENT') is not None:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polguide.settings.dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polguide.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
