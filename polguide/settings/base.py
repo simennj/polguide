e169b580641baa86e24df593eeb2456fce9b62ba
Simen <Simennj@outlook.com>
2018-06-10 22:26:55 +0200

    add STATIC_ROOT

--- polguide/settings/base.py
+++ polguide/settings/base.py
@@ -98,6 +98,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
